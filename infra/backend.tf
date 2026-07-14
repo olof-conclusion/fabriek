4cfb620d79f03e260ba692a58faa4ca128891bb6
olof-conclusion <olof.ensing@conclusion.nl>
2026-07-14 12:11:59 +0200

    b2ab: reset repository to empty

--- infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-kvk-viewer-tfstate"
-    storage_account_name = "stkvkviewertf"
-    container_name       = "tfstate"
-    key                  = "app.tfstate"
-    subscription_id      = "934cff5d-378e-4fbc-8896-1f64c29eb1ec"
-  }
-}
--- infra/modules/b2ab-app-baseline/identity.tf
+++
@@ -1,8 +0,0 @@
-module "uami" {
-  source              = "Azure/avm-res-managedidentity-userassignedidentity/azurerm"
-  version             = "0.3.3"
-  name                = local.uami_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  tags                = local.tags
-}
